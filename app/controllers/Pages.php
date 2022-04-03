ef645fc5fcdfb14badd00a2633e824f9f51f9d8e
nataliee20 <natalie1901991@miuegypt.edu.eg>
2022-04-03 16:16:03 +0200

    initial commit

---
+++ app/controllers/Pages.php
@@ -0,0 +1,21 @@
+<?php
+class Pages extends Controller
+{
+
+    public function index()
+    {
+        $viewPath = VIEWS_PATH . 'pages/Index.php';
+        require_once $viewPath;
+        $indexView = new Index($this->getModel(), $this);
+        $indexView->output();
+    }
+
+    public function about()
+    {
+        $viewPath = VIEWS_PATH . 'pages/About.php';
+        require_once $viewPath;
+        $aboutView = new About($this->getModel(), $this);
+        $aboutView->output();
+    }
+
+}
