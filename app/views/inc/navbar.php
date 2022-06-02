1e26ec3b0e53e8b9119b570af70231ffeb93cc0e
nataliee20 <natalie1901991@miuegypt.edu.eg>
2022-06-02 16:48:58 +0200

    navbar

--- app/views/inc/navbar.php
+++ app/views/inc/navbar.php
@@ -48,8 +48,8 @@
 
       </ul>
       <form class="d-flex">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
+        <input class="form-control me-2" name="find" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success"  type="submit">Search</button>
       </form>
     </div>
   </div>
