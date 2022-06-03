06ccfb42e771061515ef81825fe1ec95f8316ba3
nataliee20 <natalie1901991@miuegypt.edu.eg>
2022-06-03 03:21:15 +0200

    logout

--- app/views/inc/navbar.php
+++ app/views/inc/navbar.php
@@ -29,16 +29,17 @@
             <?php if (isset($_SESSION['user_id'])) {
               echo $_SESSION['user_name'];
             } else {
-              echo 'User';
+              echo 'Admin';
             }
             ?>
           </a>
           <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
             <?php if (isset($_SESSION['user_id'])) : ?>
-              <li><a class="dropdown-item" href="users/logout">Logout</a></li>
+              <li><a class="dropdown-item" href="<?php echo URLROOT . 'pages/admin'; ?>">admin panel</a></li>
+              <li><a class="dropdown-item" href="<?php echo URLROOT . 'users/logout'; ?>">Logout</a></li>
             <?php else : ?>
               <li><a class="dropdown-item" href="<?php echo URLROOT . 'users/login'; ?>">Login</a></li>
-              <li><a class="dropdown-item" href="<?php echo URLROOT . 'users/register'; ?>">Sign Up</a></li>
+
               <li>
                 <hr class="dropdown-divider">
               </li>
